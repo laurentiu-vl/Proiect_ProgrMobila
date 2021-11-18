420dfdd450654e95a4e4db408a740be5d3a549c2
DanielLucaci <46228296+DanielLucaci@users.noreply.github.com>
2021-11-18 16:06:24 +0200

    Created new LibGDX project

---
+++ android/src/com/frankthefrog/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.frankthefrog.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.frankthefrog.game.Frank;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Frank(), config);
+	}
+}
